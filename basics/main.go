cdb1f8c270b6581b0455086d9ecf8e167ddd7634
Anurag-Band <anuragband10@gmail.com>
2024-10-02 19:42:30 +0530

    go basics - variables, constants, loops

---
+++ basics/main.go
@@ -0,0 +1,80 @@
+package main
+
+// const outAge = 12
+
+// var newName string = "Param"
+
+//? := will not work outside main()
+
+func main() {
+	// fmt.Println("Hello World !")
+
+	//? Variables
+
+	// var name string = "Anurag"
+	// fmt.Println(name)
+
+	// age := 22
+	// fmt.Println(age)
+
+	// var location string
+	// location = "Karanja"
+	// fmt.Println(location)
+
+	//? Constants
+
+	const friend = "devang"
+
+	//? for multiple constants grouping
+
+	// const (
+	// 	port = 8080
+	// 	server = "go"
+	// )
+
+	// fmt.Println(port, server)
+
+	//? Loops - for -> only construct in go for looping (while also implemented using for)
+
+	//? while loop
+	// i := 0
+	// for i <= 5 {
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+
+	//? infinite loop
+	// for {
+	// 	fmt.Println("fuck")
+	// }
+
+	//? classic for loop
+	// for i := 0; i <= 5; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	//? continue keyword
+	// 	for i := 0; i <= 5; i++ {
+
+	// 		if i == 4 {
+	// 			continue
+	// 		}
+	// 		fmt.Println(i)
+	// 	}
+
+	//? break keyword
+	// for i := 0; i <= 5; i++ {
+
+	// 	if i == 4 {
+	// 		break
+	// 	}
+	// 	fmt.Println(i)
+	// }
+
+	//? range keyword
+	// for i := range 10 {
+	// 	fmt.Println(i)
+	// }
+
+	
+}
